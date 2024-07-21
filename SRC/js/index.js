1922d61110e1a38441ca8b9092942b3d5a0180bd
Eu-Luca43 <lucaspradotra@gmail.com>
2024-07-21 18:34:26 -0300

    projeto-mario

---
+++ SRC/js/index.js
@@ -0,0 +1,21 @@
+/*
+
+
+
+
+
+*/
+
+console.log(document.querySelector('.botao-trailer'));
+const BotaoTrailer = document.querySelector('.botao-trailer');
+BotaoTrailer.addEventListener("click", () => { 
+    console.log("clicou no botão veja o trailer")});
+
+
+
+
+
+console.log(document.querySelector('.Home'));
+const Home = document.querySelector('.Home');
+Home.addEventListener("click", () => { 
+    console.log("Home")});
